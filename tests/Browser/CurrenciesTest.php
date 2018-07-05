b550a22fad4ff82141342218ccd85266a7d431c6
antarus66 <antarus66@gmail.com>
2018-07-05 04:32:43 +0300

    Added testing lots on the currency page.

---
+++ tests/Browser/CurrenciesTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+
+class CurrenciesTest extends DuskTestCase
+{
+    public function testGoToCurrencyPage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/currencies')
+                ->assertSee('Bitcoin')
+                ->clickLink('Bitcoin')
+                ->assertPathIs('/currencies/1');
+        });
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/currencies')
+                ->assertSee('Dogecoin')
+                ->clickLink('Dogecoin')
+                ->assertPathIs('/currencies/2');
+        });
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/currencies')
+                ->assertSee('Litecoin')
+                ->clickLink('Litecoin')
+                ->assertPathIs('/currencies/3');
+        });
+    }
+
+    public function testContainsLots()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/currencies')
+                ->assertSee('Bitcoin')
+                ->clickLink('Bitcoin')
+                ->assertPathIs('/currencies/1')
+                ->assertSee('6631')
+                ->assertSee('6633')
+                ->assertSee('6636');
+        });
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/currencies')
+                ->assertSee('Dogecoin')
+                ->clickLink('Dogecoin')
+                ->assertPathIs('/currencies/2')
+                ->assertSee('0.002695')
+                ->assertSee('0.0027')
+                ->assertSee('0.00278');
+        });
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/currencies')
+                ->assertSee('Litecoin')
+                ->clickLink('Litecoin')
+                ->assertPathIs('/currencies/3')
+                ->assertSee('85')
+                ->assertSee('84')
+                ->assertSee('88');
+        });
+    }
+}
