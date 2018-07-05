463c6907b61ef24eb5c6b3a2ebd062329b81e3c9
antarus66 <antarus66@gmail.com>
2018-07-05 03:38:50 +0300

    Added a migration for a currency foreign key without restricting it.

--- database/migrations/2018_07_04_225131_add_currency_index_to_lot_table.php
+++ database/migrations/2018_07_04_225131_add_currency_index_to_lot_table.php
@@ -14,9 +14,7 @@ class AddCurrencyIndexToLotTable extends Migration
     public function up()
     {
         Schema::table('lot', function (Blueprint $table) {
-            $table->integer('currency_id')->unsigned()->default(0);
-            $table->foreign('currency_id')->references('id')->on('currencies')
-                ->onDelete('restrict');
+            $table->integer('currency_id')->unsigned();
         });
     }
 
@@ -28,7 +26,6 @@ class AddCurrencyIndexToLotTable extends Migration
     public function down()
     {
         Schema::table('lot', function (Blueprint $table) {
-            $table->dropForeign('currency_id');
             $table->dropColumn('currency_id');
         });
     }
